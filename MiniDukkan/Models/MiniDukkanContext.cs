b5158f7de638044237e3757fc559e476b1743353
ahmet said erdem <erdemahmet750@gmail.com>
2024-01-12 15:47:05 +0300

    Frameworkler yüklendi veritabanı ile bağlantı sağlandı

---
+++ MiniDukkan/Models/MiniDukkanContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MiniDukkan.Models
+{
+    public class MiniDukkanContext:DbContext
+    {
+        public MiniDukkanContext(DbContextOptions<MiniDukkanContext> options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<Product> Products { get; set; }
+        
+    }
+}
--- MiniDukkan/Program.cs
+++ MiniDukkan/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MiniDukkan.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MiniDukkanContext>(opts => opts.UseSqlServer(builder.Configuration["ConnectionStrings:MiniDukkanConnection"]));
 
 var app = builder.Build();
 
@@ -12,6 +17,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
