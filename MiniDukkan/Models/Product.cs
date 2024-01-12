2d3115e689bcac06a5ef13bd7765c2824ee0180a
ahmet said erdem <erdemahmet750@gmail.com>
2024-01-12 14:23:31 +0300

    Add project files.

---
+++ MiniDukkan/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MiniDukkan.Models
+{
+    public class Product
+    {
+        public long UrunID{ get; set; }
+        public string UrunAd { get; set; }
+        public string Aciklama { get; set; }
+
+        [Column(TypeName ="decimal(6,2)")]
+        public decimal Fiyat { get; set; }
+        public string Kategori { get; set; }
+    }
+}
