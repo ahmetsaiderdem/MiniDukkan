82b8850f892647bfcf549b939acb8eba245fca5c
ahmet said erdem <erdemahmet750@gmail.com>
2024-01-13 15:53:14 +0300

    Her bir ürünün sayfalarına karşılık gelen öğeler ile ilgili div elementinin içini doldurdum

---
+++ MiniDukkan/Altyapi/SayfaLinkTagHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MiniDukkan.Models.ViewModels;
+
+namespace MiniDukkan.Altyapi
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class SayfaLinkTagHelper: TagHelper
+    {
+        private IUrlHelperFactory _urlHelperFactory;
+
+        public SayfaLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelperFactory = urlHelperFactory;
+        }
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public SayfalamaBilgi SayfaModel { get; set; }
+
+        public string SayfaAction { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder sonuc = new TagBuilder("div");
+            for (int i = 0; i < SayfaModel.ToplamSayfalar; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes["href"]=urlHelper.Action(SayfaAction,new {urunSayfa =i});
+                tag.InnerHtml.Append(i.ToString());
+                sonuc.InnerHtml.AppendHtml(tag);
+
+
+
+            }
+            output.Content.AppendHtml(sonuc.InnerHtml);
+        }
+    }
+}
---
+++ MiniDukkan/Models/ViewModels/SayfalamaBilgi.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniDukkan.Models.ViewModels
+{
+    public class SayfalamaBilgi
+    {
+        public int ToplamUrunSayisi { get; set; }
+        public int SayfaBasiGosterilecekUrun { get; set; }
+        public int GuncelSayfa { get; set; }
+        public int ToplamSayfalar => (int)Math.Ceiling((decimal)ToplamUrunSayisi / SayfaBasiGosterilecekUrun);
+
+    }
+}
